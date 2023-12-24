e9037ae16cb31fac2ba39014fbc8373acd395b42
SuperiorTabby <117610080+SuperiorTabby@users.noreply.github.com>
2023-12-24 15:58:54 -0800

    Add files via upload

---
+++ src/client/java/com/superiortabby/vegetables/TabbysVegetablesClient.java
@@ -0,0 +1,10 @@
+package com.superiortabby.vegetables;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TabbysVegetablesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/superiortabby/vegetables/TabbysVegetablesDataGenerator.java
@@ -0,0 +1,11 @@
+package com.superiortabby.vegetables;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TabbysVegetablesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
