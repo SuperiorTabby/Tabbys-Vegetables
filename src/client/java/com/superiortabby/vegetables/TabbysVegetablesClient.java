2c987a6c6886cd34dbe019f9bb033983dce93402
SuperiorTabby <colesonwhiting@gmail.com>
2024-01-05 12:17:27 -0800

    The Turnip Crop in Glory

--- src/client/java/com/superiortabby/vegetables/TabbysVegetablesClient.java
+++ src/client/java/com/superiortabby/vegetables/TabbysVegetablesClient.java
@@ -3,7 +3,7 @@ package com.superiortabby.vegetables;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
-import java.awt.image.renderable.RenderableImage;
+
 
 import static com.superiortabby.vegetables.TabbysVegetables.TURNIP_CROP;
 
